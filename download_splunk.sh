b2bb06bc58f121602b3d49716955bc508c0fc0ce
lsav00 <savidge00@gmail.com>
2025-09-01 22:42:30 -0700

    fix installer

--- download_splunk.sh
+++ download_splunk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTALLER_URL=$(grep -v '^#'  splunk_installer.txt | tr -d '"' | tr -d "'" | xargs)
+INSTALLER_URL="https://download.splunk.com/products/splunk/releases/10.0.0/linux/splunk-10.0.0-e8eb0c4654f8-linux-amd64.tgz"
 TARGET_DIR=$HOME
 FILENAME=$(basename "$INSTALLER_URL")
 
